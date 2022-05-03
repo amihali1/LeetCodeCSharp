892d8187f12ab369bf6ccbd2257af0f0631cebed
Andy Mihalik <andymihalik@gmail.com>
2022-05-03 15:04:39 -0500

    Roman numerals to integers

---
+++ RomanToInt/RomanToInt/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RomanToInt;
+
+var solution = new Solution();
+
+Console.WriteLine("Testing string \"III\"");
+var result = solution.RomanToInt("III");
+Console.WriteLine($"Expected Result: 3   | Actual Result: {result}");
+
+Console.WriteLine("Testing string \"LVIII\"");
+result = solution.RomanToInt("LVIII");
+Console.WriteLine($"Expected Result: 58   | Actual Result: {result}");
+
+Console.WriteLine("Testing string \"MCMXCIV\"");
+result = solution.RomanToInt("MCMXCIV");
+Console.WriteLine($"Expected Result: 1994   | Actual Result: {result}");
---
+++ RomanToInt/RomanToInt/Solution.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanToInt
+{
+    internal class Solution
+    {
+        public int RomanToInt(string s)
+        {
+            int result = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                var curChar = new RomanNumeral(s[i]);
+                var nextChar = i < s.Length - 1 ? new RomanNumeral(s[i + 1]) : null;
+
+                if (nextChar != null && nextChar.intValue > curChar.intValue)
+                {
+                    result += (nextChar.intValue - curChar.intValue);
+                    i++;
+                }
+                else
+                    result += curChar.intValue;
+            }
+
+            return result;
+        }
+    }
+
+    internal class RomanNumeral
+    {
+        internal char stringValue;
+        internal int intValue;
+
+        public RomanNumeral(char character)
+        {
+            stringValue = character;
+            intValue = character == 'M' ? 1000 :
+                       character == 'D' ? 500 :
+                       character == 'C' ? 100 :
+                       character == 'L' ? 50 :
+                       character == 'X' ? 10 :
+                       character == 'V' ? 5 :
+                       1;                       
+        }
+    }
+}
