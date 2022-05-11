0fee234b7f456f07ca70575f1e3a8aa39133cebc
Andy Mihalik <andymihalik@gmail.com>
2022-05-10 22:19:55 -0500

    Tried to get this to work with a stack. Not passing all cases but I may come back to this one...

--- Pattern132/Pattern132/Program.cs
+++ Pattern132/Pattern132/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Given an array of n integers nums, 
+a 132 pattern is a subsequence of three integers
+nums[i], nums[j] and nums[k] such that 
+i < j < k and nums[i] < nums[k] < nums[j].
+
+Return true if there is a 132 pattern in nums, otherwise, return false.
+*/
+
+using Pattern132;
+
+var solution = new Solution();
+
+Console.WriteLine("Testing [1,2,3,4]");
+Console.WriteLine("Expected Result: False | Actual Result: " + solution.Find132Pattern(new int[] { 1, 2, 3, 4 }));
+
+Console.WriteLine("Testing [3,1,4,2]");
+Console.WriteLine("Expected Result: True | Actual Result: " + solution.Find132Pattern(new int[] { 3, 1, 4, 2 }));
+
+Console.WriteLine("Testing [-1,3,2,0]");
+Console.WriteLine("Expected Result: True | Actual Result: " + solution.Find132Pattern(new int[] { -1, 3, 2, 0 }));
---
+++ Pattern132/Pattern132/Solution.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern132
+{
+    internal class Solution
+    {
+
+        internal bool Find132Pattern(int[] nums)
+        {            
+            var leftMins = new Stack<int>();
+
+            leftMins.Push(nums[0]);
+            for (int i = 1; i < nums.Length; i++)
+            {
+                leftMins.Push(Math.Min(nums[i - 1], nums[i]));
+            }
+
+            var rightMins = new Stack<int>();
+
+            for (int i = nums.Length - 1; i > 0; i--)
+            {
+                var leftMin = leftMins.ElementAt(i-1);
+                var middleNum = nums[i];
+
+                while (rightMins.Any() && rightMins.Peek() <= leftMin) 
+                    rightMins.Pop();
+                
+                if (rightMins.Any() && rightMins.Peek() < middleNum) 
+                    return true;
+                
+                rightMins.Push(nums[i]);
+            }
+            return false;
+        }
+
+        // TOO SLOW
+        //public bool Find132pattern(int[] nums)
+        //{
+        //    for (int i = 0; i < nums.Length - 2; i++)
+        //    {
+        //        for (int j = i + 1; j < nums.Length - 1; j++)
+        //        {
+        //            if (nums[i] < nums[j])
+        //            {
+        //                for (int k = j + 1; k < nums.Length; k++)
+        //                {
+
+        //                    if (nums[j] > nums[k] && nums[k] > nums[i])
+        //                        return true;
+
+        //                }
+        //            }
+        //        }
+        //    }
+        //    return false;
+        //}
+    }
+}
